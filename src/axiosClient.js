e7b753eedcc93dd9231735151fa94564e2ec5c5d
sanjana <sanjanabaskar05@gmail.com>
2025-11-27 13:31:00 +0530

    modified api

--- src/axiosClient.js
+++ src/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://lms-back-nh5h.onrender.com/api",
   withCredentials: true,
 });
 
